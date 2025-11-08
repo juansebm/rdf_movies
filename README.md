3f84594858ea06a14516f73fe26a037b7b3b6cc3
J. Sebastian Miño <109161573+juansebm@users.noreply.github.com>
2025-11-08 12:57:11 -0300

    Data + Script

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # rdf_movies
 
+```
+netflix_titles.csv
+        │
+        ▼
+ title.basics.tsv → merge_titles.py → netflix_imdb_movies.csv
+        ▲
+        │
+ title.ratings.tsv
+```
---
+++ merge_titles.py
@@ -0,0 +1,19 @@
+import pandas as pd
+from rich.progress import Progress
+
+with Progress(transient=True) as progress:
+    task = progress.add_task('', total=4)
+    netflix = pd.read_csv('netflix_titles.csv')
+    progress.advance(task)
+    movies = netflix[netflix['type'].eq('Movie')]
+    basics = pd.read_csv('title.basics.tsv', sep='\t', dtype=str, na_values='\\N', usecols=['tconst', 'primaryTitle'])
+    progress.advance(task)
+    ratings = pd.read_csv('title.ratings.tsv', sep='\t', dtype={'tconst': str}, na_values='\\N', usecols=['tconst', 'averageRating'])
+    progress.advance(task)
+    merged = movies.merge(basics, left_on='title', right_on='primaryTitle')
+    merged = merged.merge(ratings, on='tconst', how='left')
+    merged = merged.drop_duplicates(subset='show_id')
+    cols = [columna for columna in movies.columns if columna != 'description']
+    merged[cols + ['tconst', 'averageRating']].to_csv('netflix_imdb_movies.csv', index=False)
+    progress.advance(task)
+
